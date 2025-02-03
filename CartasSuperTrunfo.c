ab230282ecd7719246d67e613db94586d7c61ceb
thayna1113 <thaynas1113@gmail.com>
2025-02-03 15:42:55 +0000

    de volta ao 0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,44 +3,52 @@
 int main() {
 
 
-  char estado[4];
+  char estado;
+  int codigo de cartas;
   char cidade[50];
-  double populacao;
-  double area;
-  double pib;
-  int pontos; //turisticos
-  double divisao; 
+  int populacao;
+  float area;
+  float pib;
+  int pontos turisticos;
+  float densidade populacional;
+  float PIB per capita;
+  float super poder;
 
 
   printf("digite o estado:\n");
-  scanf(" %3s", estado);
+  scanf("%c", estado);
+
+
+  printf("digite o codigo da carta:\n");
+  scanf("%d", cogigo da carta);
 
 
   printf("digite a cidade:\n");
-  scanf(" %s", cidade);
+  scanf("%s", cidade);
 
 
   printf("digite a população:\n");
-  scanf(" %lf", &populacao);
+  scanf("%d", &populacao);
 
 
   printf("digite a area:\n");
-  scanf(" %lf", &area);
+  scanf("%f", &area);
 
 
   printf("digite o pib:\n");
-  scanf(" %lf", &pib);
+  scanf("%f", &pib);
 
 
   printf("digite o numero de ponto turístico:\n");
-  scanf(" %d", &pontos);
+  scanf("%d", &pontos turisticos);
 
-  //operaçao dividir
-  divisao = (pib / populacao);
-  
-  printf("O calculo é: %.2f \n", divisao);
-  printf("estado: %s - cidade: %s\n", estado, cidade);
-  printf("populacao: %.2f - area: %.2f - pib: %.2f - pontos %d \n", populacao, area, pib, pontos);
+
+  printf("digite a densidade populacional:\n");
+  scanf("%f", &densidade populacional);
+
+  printf("digite o pib per capita:\n");
+  scanf("%f", &pib per capita);
+
 
     return 0;
 }
