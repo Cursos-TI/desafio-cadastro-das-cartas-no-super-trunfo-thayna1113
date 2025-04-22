6f28caebd3fac3f31b15160aeb27dc9b36818885
thayna1113 <thaynas1113@gmail.com>
2025-04-22 21:28:27 +0000

    cartassupertrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@ int main() {
 
 
   char estado1, estado2;
-  int codigo1[5], codigo2[5];
+  char codigo1[5], codigo2[5];
   char cidade1[50], cidade2[50];
   int populacao1, populacao2;
   float area1, area2;
@@ -19,13 +19,13 @@ int main() {
 
 
   printf("digite o codigo da carta1:\n");
-  scanf("%s", codigo1);
+  scanf(" %s", codigo1);
   printf("digite o estado:\n");
   scanf(" %c", &estado1);
   printf("digite a cidade:\n");
-  scanf( " %s", cidade1);
-  printf("digite a população:\n");
-  scanf( " %d", &populacao1);
+  scanf(" %s", cidade1);
+  printf("digite a populacao:\n");
+  scanf("%d", &populacao1);
   printf("digite a area:\n");
   scanf("%f", &area1);
   printf("digite o pib:\n");
@@ -34,16 +34,43 @@ int main() {
   scanf("%d", &pontosturisticos1);
 
 
-  printf("o codigo da primeira carta é: %s \n", codigo1);
-  printf("o estado é:%c \n", &estado1);
-  printf("a cidade é:%s  \n", cidade1);
-  printf("a populacao é:%d \n", &populacao1);
-  printf("a area é:%f  \n", &area1);
-  printf("o pib é:%f \n", &pib1);
-  printf("os pontos turistico são:%d \n", &pontosturisticos1);
+printf("o codigo da primeira carta é: %s \n", codigo1);
+printf("o estado é:%c \n", estado1);
+printf("a cidade é:%s  \n", cidade1);
+printf("a populacao é:%d \n", populacao1);
+printf("a area é:%f  \n", area1);
+printf("o pib é:%f \n", pib1);
+printf("os pontos turistico são:%d \n", pontosturisticos1);
+
+
 
   //declaraçao de variaveis carta 2
 
+  printf("digite o codigo da carta2:\n");
+  scanf(" %s", codigo2);
+  printf("digite o estado:\n");
+  scanf(" %c", &estado2);
+  printf("digite a cidade:\n");
+  scanf(" %s", cidade2);
+  printf("digite a populacao:\n");
+  scanf("%d", &populacao2);
+  printf("digite a area:\n");
+  scanf("%f", &area2);
+  printf("digite o pib:\n");
+  scanf("%f", &pib2);
+  printf("digite o numero de pontos turisticos:\n");
+  scanf("%d", &pontosturisticos2);
+
+
+
+  printf("o codigo da primeira carta é: %s \n", codigo2);
+  printf("o estado é:%c \n", estado2);
+  printf("a cidade é:%s  \n", cidade2);
+  printf("a populacao é:%d \n", populacao2);
+  printf("a area é:%f  \n", area2);
+  printf("o pib é:%f \n", pib2);
+  printf("os pontos turistico são:%d \n", pontosturisticos2);
+ 
 
     return 0;
 }
